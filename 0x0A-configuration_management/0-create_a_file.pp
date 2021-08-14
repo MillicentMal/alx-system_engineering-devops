fb31834d4dd415b31b4434e63507f955fd7c5de9
MillicentMal <millicentmalinga19@gmail.com>
2021-08-14 12:58:45 +0000

    Task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -17,9 +17,16 @@ content => 'I love Puppet'
 >>>>>>> f548fc4 (Task 0)
 =======
 file { '/tmp/holberton':
+<<<<<<< HEAD
  perm    => 0744,
  group   => www-data,
  owner   => www-data,
  content => 'I love Puppet'
 >>>>>>> 6301ba5 (Task 0)
+=======
+  perm    => 0744,
+  group   => www-data,
+  owner   => www-data,
+  content => 'I love Puppet'
+>>>>>>> 7b8c747 (Task 0)
 }
