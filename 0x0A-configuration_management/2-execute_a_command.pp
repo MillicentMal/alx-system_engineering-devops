2630fd58629cd7ac356f4048e416144ccf41974c
MillicentMal <millicentmalinga19@gmail.com>
2021-08-14 13:19:19 +0000

    Task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,10 @@
 #kills the process killmenow
 
+<<<<<<< HEAD
 exec { 'kill':
+=======
+exec { 'kill'
+>>>>>>> 35761b3 (Task 2)
   command => 'pkill -f killmenow',
   path    => ['/usr/bin', 'usr/sbin']
 }
