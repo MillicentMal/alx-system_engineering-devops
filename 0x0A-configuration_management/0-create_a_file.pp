712ef9ad3ddfff6257a3798cefc2121a6660ae05
MillicentMal <millicentmalinga19@gmail.com>
2021-08-14 13:03:45 +0000

    Task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 #creates a file in /tmp/holberton
 
 file { '/tmp/holberton':
-  perm    => 0744,
+  ensure  => 'file',
+  mode    => 0744,
   group   => www-data,
   owner   => www-data,
   content => 'I love Puppet'
