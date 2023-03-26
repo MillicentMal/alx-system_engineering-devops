1055d234ece4da4451f777a608afb445c0530453
MillicentMal <millicentmalinga19@gmail.com>
2023-03-26 15:55:36 -0700

    Changes task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,46 +1,8 @@
 #creates a file in /tmp/holberton
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 file { '/tmp/holberton':
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
   mode    => '0744',
   group   => 'www-data',
   owner   => 'www-data',
   content => 'I love Puppet'
-=======
-file {
-/tmp/holberton:
-perm => 0744,
-group => www-data,
-owner => www-data,
-content => 'I love Puppet'
->>>>>>> f548fc4 (Task 0)
-=======
-file { '/tmp/holberton':
-<<<<<<< HEAD
- perm    => 0744,
- group   => www-data,
- owner   => www-data,
- content => 'I love Puppet'
->>>>>>> 6301ba5 (Task 0)
-=======
-  perm    => 0744,
-=======
-  ensure  => 'file',
-=======
->>>>>>> 4cb1207 (Task 0)
-  mode    => 0744,
->>>>>>> 360947a (Task 0)
-  group   => www-data,
-  owner   => www-data,
-=======
-  mode    => '0744',
-  group   => 'www-data',
-  owner   => 'www-data',
->>>>>>> f75eb20 (Task 0)
-  content => 'I love Puppet'
->>>>>>> 7b8c747 (Task 0)
 }
