98f61959d2339f3ec32874c47c9a02f612ee6fa8
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2023-03-27 01:04:13 +0200

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,14 +1,7 @@
 #kills the process killmenow
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 exec { 'kill':
-=======
-exec { 'kill'
->>>>>>> 35761b3 (Task 2)
-=======
-exec { 'kill':
->>>>>>> 0878ebc (Task 0)
+
   command => 'pkill -f killmenow',
   path    => ['/usr/bin', 'usr/sbin']
 }
